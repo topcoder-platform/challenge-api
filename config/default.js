a66257cf0f6bb9112226c13d278e87e4629c0e65
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-09-23 12:15:28 +0300

    Add API version

--- config/default.js
+++ config/default.js
@@ -5,6 +5,7 @@
 module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   PORT: process.env.PORT || 3000,
+  API_VERSION: process.env.API_VERSION || 'v5',
   AUTH_SECRET: process.env.AUTH_SECRET || 'mysecret',
   VALID_ISSUERS: process.env.VALID_ISSUERS || '["https://api.topcoder-dev.com", "https://api.topcoder.com", "https://topcoder-dev.auth0.com/"]',
 
