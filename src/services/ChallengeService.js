7ff0fa80fb6ec25145fcc9cd7e04f74a364ef071
James Cori <jamescori@gmail.com>
2020-08-03 14:17:24 -0400

    Fixing task lookup

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -946,7 +946,7 @@ async function getChallenge (currentUser, id) {
   // Check if challenge is task and apply security rules
   if (_.get(challenge, 'task.isTask', false) && _.get(challenge, 'task.isAssigned', false)) {
     const skipAccessCheck = !currentUser ? false : currentUser.isMachine || helper.hasAdminRole(currentUser)
-    if (!skipAccessCheck && currentUser.userId !== _.get(challenge, 'task.memberId')) {
+    if (!skipAccessCheck && currentUser && currentUser.userId !== _.get(challenge, 'task.memberId')) {
       throw new errors.ForbiddenError(`You don't have access to view this challenge`)
     }
   }
