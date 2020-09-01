746dfc7421b29204bb2789ab7c8fc45008515b44
James Cori <jamescori@gmail.com>
2020-09-01 14:31:34 -0400

    Fixing it to filter on key, not id

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -277,11 +277,11 @@ async function searchChallenges (currentUser, criteria) {
   if (criteria.events) {
     if (criteria.includeAllEvents) {
       for (const e of criteria.events) {
-        boolQuery.push({ match_phrase: { 'events.id': e } })
+        boolQuery.push({ match_phrase: { 'events.key': e } })
       }
     } else {
       for (const e of criteria.events) {
-        shouldQuery.push({ match: { 'events.id': e } })
+        shouldQuery.push({ match: { 'events.key': e } })
       }
     }
   }
