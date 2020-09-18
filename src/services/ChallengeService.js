3c794cd0d0b36934e0f27f0fda25380d6422b905
Thomas Kranitsas <kranitsasthomas@gmail.com>
2020-09-18 13:49:52 +0300

    change isUndefined with isEmpty

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -1425,7 +1425,7 @@ async function update (currentUser, challengeId, data, userToken, isFull) {
   const { track, type } = await validateChallengeData(_.pick(challenge, ['trackId', 'typeId']))
 
   if (_.get(type, 'isTask')) {
-    if (!_.isUndefined(_.get(challenge, 'task.memberId'))) {
+    if (!_.isEmpty(_.get(challenge, 'task.memberId'))) {
       const challengeResources = await helper.getChallengeResources(challengeId)
       const registrants = _.filter(challengeResources, r => r.roleId === config.SUBMITTER_ROLE_ID)
       if (!_.find(registrants, r => _.toString(r.memberId) === _.toString(_.get(challenge, 'task.memberId')))) {
