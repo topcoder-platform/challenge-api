d716e8bc34f4b8f974f872b9a80792244b992d6f
Thomas Kranitsas <kranitsasthomas@gmail.com>
2020-10-23 20:19:43 +0300

    fix metadata query format

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -174,7 +174,12 @@ async function searchChallenges (currentUser, criteria) {
       // Parse and use metadata key
       if (!_.isUndefined(criteria[key])) {
         const metaKey = key.split('meta.')[1]
-        boolQuery.push({ match_phrase: { [`metadata.${metaKey}`]: criteria[key] } })
+        boolQuery.push({
+          must: [
+            { match_phrase: { 'metadata.name': metaKey } },
+            { match_phrase: { 'metadata.value': criteria[key] } }
+          ]
+        })
       }
     }
   })
