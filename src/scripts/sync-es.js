d60f7b056ac37c29abd85f1663d9b0ff1b56efe1
Rakib Ansary <rakibansary@gmail.com>
2023-02-21 17:01:27 +0600

    fix: script to migrate challenge records to dynamo-db

--- src/scripts/sync-es.js
+++ src/scripts/sync-es.js
@@ -5,6 +5,8 @@
 const config = require("config");
 const logger = require("../common/logger");
 const helper = require("../common/helper");
+const models = require("../models");
+const _ = require("lodash");
 
 const esClient = helper.getESClient();
 
@@ -12,13 +14,28 @@ const esClient = helper.getESClient();
  * Migrate records from DB to ES
  */
 async function migrateRecords() {
-  const result = await helper.scan("Challenge");
-  for (const challenge of result) {
+  let results = await models["Challenge"].scan().exec();
+  let lastKey = results.lastKey;
+
+  for (const challenge of results) {
     await esClient.update({
       index: config.get("ES.ES_INDEX"),
       id: challenge.id,
       body: { doc: challenge, doc_as_upsert: true },
     });
+  }
+
+  while (!_.isUndefined(results.lastKey)) {
+    const results = await models["Challenge"].scan().startAt(lastKey).exec();
+    for (const challenge of results) {
+      await esClient.update({
+        index: config.get("ES.ES_INDEX"),
+        id: challenge.id,
+        body: { doc: challenge, doc_as_upsert: true },
+      });
+    }
+
+    lastKey = results.lastKey;
   }
 }
 
