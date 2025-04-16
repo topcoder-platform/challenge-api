84693e5343e57fc8f92ee444544e10f086ade205
whereishammer <whatisbestinlife@163.com>
2025-04-16 21:17:46 +0800

    fix merge conflict in ChallengeService test

--- test/unit/ChallengeService.test.js
+++ test/unit/ChallengeService.test.js
@@ -373,11 +373,6 @@ describe('challenge service unit tests', () => {
         page: 1,
         perPage: 10,
         id: id,
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> bf8ac4b (fix Attachment tests)
         typeId: testChallengeData.typeId,
         name: testChallengeData.name.substring(2).trim(),
         description: testChallengeData.description,
